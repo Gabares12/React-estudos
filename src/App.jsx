d0ac034655bef9c9044c5e31e4919e51aa9bd561
Gabriel Tavares <Gabriel.Tavares@ciazaffari.com.br>
2026-02-10 08:07:49 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento () {
+
+  return(
+    
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+    </main>
+  )
+}
+
+export default App
